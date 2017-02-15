738dd540363d705d60870fa1b648fa940b051efa
Nicole Johnson <nicole@localhost.localdomain>
2017-02-15 18:45:27 +0200

    Removed and reordered routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,11 +52,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 //$route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 //$route['translate_uri_dashes'] = FALSE;
-
+$route['default_controller'] = 'welcome';
 
 $route['scores/create'] = 'scores/create';
-$route['scores/(:any)'] = 'scores/view/$1';
 $route['scores'] = 'scores';
-
-$route['default_controller'] = 'welcome';
 $route['tictactoe'] = 'tictactoe';
