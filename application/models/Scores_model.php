61776ae136b8acab91c9b88caf1754e5bb69d246
Nicole Johnson <nicole@localhost.localdomain>
2017-02-17 13:07:08 +0200

    New function to retrieve results within date range

--- application/models/Scores_model.php
+++ application/models/Scores_model.php
@@ -32,4 +32,20 @@ class Scores_model extends CI_Model {
             log_message('info', 'new game scores added in db > data: '.print_r($data, true));
             return $this->db->insert('scores', $data);
         }
+    public function get_dated_scores($start, $end)
+    {
+        $start_row = 0;
+        $limit = 0;
+
+        $this->db->order_by("id", "DESC");
+        $this->db->where('created_at >=', $start);
+        $this->db->where('created_at <=', $end);
+
+        $query = $this->db->get('scores', $limit, $start_row);
+
+        $scores = $query->result_array();
+
+        return $scores;
+    }
+
 }
