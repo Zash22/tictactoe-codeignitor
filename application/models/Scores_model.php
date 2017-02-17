5826630e583369ad53e156f246256aa4ab2dce37
Nicole Johnson <nicole@localhost.localdomain>
2017-02-17 13:07:33 +0200

    reformatting

--- application/models/Scores_model.php
+++ application/models/Scores_model.php
@@ -1,37 +1,27 @@
 <?php
-class Scores_model extends CI_Model {
 
-        public function __construct()
-        {
-                $this->load->database();
-        }
-    
-        public function get_scores($limit)
-        {
-                        $limit;
-                        $start_row=0;
+class Scores_model extends CI_Model
+{
 
-                        $this->db->order_by("id", "DESC");
-                        $query = $this->db->get('scores', $limit, $start_row);
+    public function __construct()
+    {
+        $this->load->database();
+    }
 
-                        $scores =  $query->result_array();
+    public function get_scores($limit)
+    {
+        $limit;
+        $start_row = 0;
 
-                        return $scores;
-        }
+        $this->db->order_by("id", "DESC");
+        $query = $this->db->get('scores', $limit, $start_row);
 
-        public function set_scores()
-        {
-            $this->load->helper('url');
+        $scores = $query->result_array();
 
-            $data = array(
+        return $scores;
+    }
 
-                'player1' => $this->input->post('player1'),
-                'player2' => $this->input->post('player2'),
-                'winner' => $this->input->post('winner')
-            );
-            log_message('info', 'new game scores added in db > data: '.print_r($data, true));
-            return $this->db->insert('scores', $data);
-        }
+
     public function get_dated_scores($start, $end)
     {
         $start_row = 0;
@@ -48,4 +38,17 @@ class Scores_model extends CI_Model {
         return $scores;
     }
 
+    public function set_scores()
+    {
+        $this->load->helper('url');
+
+        $data = array(
+
+            'player1' => $this->input->post('player1'),
+            'player2' => $this->input->post('player2'),
+            'winner' => $this->input->post('winner')
+        );
+        log_message('info', 'new game scores added in db > data: ' . print_r($data, true));
+        return $this->db->insert('scores', $data);
+    }
 }
